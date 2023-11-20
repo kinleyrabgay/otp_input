6810c692e95eb91279783fdbc26aa3d1b8ae2704
kinleyrabgay <fsd.rabgay@gmail.com>
2023-11-20 11:58:39 +0600

    update_otp_name

--- lib/src/otp_input_widget.dart
+++ lib/src/otp_input_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class OtpInputField extends StatelessWidget {
+  const OtpInputField({super.key});
 
   @override
   Widget build(BuildContext context) {
